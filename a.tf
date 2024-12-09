06e395d890ad0c0b85b6a5b8721df75cd4e55f73
Manu Chandrasekhar <manuchn@amazon.com>
2024-12-09 12:30:33 -0500

    test

---
+++ a.tf
@@ -0,0 +1,13 @@
+data "tfe_variable_set" "test" {
+  name         = "stacks_infra2_ou"
+  organization = "wellsiau-org"
+}
+
+data "tfe_variables" "test" {
+  variable_set_id = data.tfe_variable_set.test.id
+}
+
+
+outputs "vars" {
+    value= data.tfe_variables.test
+}
